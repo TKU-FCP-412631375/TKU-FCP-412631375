e7a30e130dd19acdb56de10d53d581a4355ace7a
TKU-FCP-412631375 <lalalazoe88396@gmail.com>
2024-07-02 12:04:36 +0800

    我的第一支java程式

---
+++ ch2_1.java
@@ -0,0 +1,13 @@
+//ch2_1,簡單的java程式
+public class ch2_1{   //定義public類別ch2_1
+    public static void main(String args[]){    //main()函數，主程式開始
+        int num1=2;            //宣告整數num
+        int num2=3;
+        float num3=4;
+        String num4="五";
+      System.out.println(num1+"cats are runing");  //印出字串內容
+      System.out.println(num2+"dogs are runing");
+      System.out.println(num3+"birds are runing");
+      System.out.println(num4+"birds are runing");
+    }
+}
---
+++ simple.java
@@ -0,0 +1,7 @@
+public class simple{
+    public static void main(String args[]){
+      System.out.println("hello java!");
+      System.out.println("0702");
+
+    }
+}
